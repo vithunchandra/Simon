582b6cf9c2051670e130d37803cb1bf98b380fe5
dragoox <datawolfxxx4@gmail.com>
2022-05-31 09:54:15 +0700

    code for hit me skill

--- Simon/src/Pokemon/Skill/HitMe.java
+++ Simon/src/Pokemon/Skill/HitMe.java
@@ -17,21 +17,44 @@ public class HitMe extends Skill{
     
     @Override
     public String use(Pokemon player, Pokemon enemy, boolean usedByPlayer) {
-        int heal = player.getDamage() * 10;
-        player.healed(heal);
-        int damage = (int) (player.getDamage()*1);
+        //damage based on player hp
+        int damage = player.getDamage();
+        if(player.getHp() <= player.getMaxHp()*2/10) {
+            damage = player.getDamage()*3;
+        }
+        else if(player.getHp() <= player.getMaxHp()*5/10) {
+            damage = (int) (player.getDamage()*1.5);
+        }
+        
+        
+        //checking effectivity
+        Double multType = player.getTypeMultiplier(enemy);
+        damage = (int)(damage*multType);
+        
+        //apply damage
         enemy.damaged(damage);
         
-        if(usedByPlayer){
-            return "enemy damaged by " + damage + "Player +" + heal + "HP";
-        }else{
-            return "enemy damaged by " + damage +"Enemy +" + heal + "HP";
+        //return string for effectivity
+        String returnString = "";
+        if(multType >= 1.2) {
+            returnString = returnString + "it is super effective\n";
         }
+        else if(multType <= 0.8) {
+            returnString = returnString + "it is not effective\n";
+        }
+        
+        //return string to report damage dealt
+        if(usedByPlayer) {
+            returnString += "enemy damaged by " + damage;
+        } else {
+            returnString += "player damaged by " + damage;
+        }
+        return returnString;
     }
     
     @Override
     public String getDescription() {
-        return "This skill will heal the caster based by 1000% of pokemon damage.";
+        return "This skill will deal 150% damage when caster health bellow 50% and 300% damage when bellow 20%";
     }
     
 }
