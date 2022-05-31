2611ef096e2ce5e0a579cc3f644fea3727d924ed
harakiri@gmail.com <harakirimadamada@gmail.com>
2022-05-31 11:29:12 +0900

    add Steal !

--- Simon/src/Pokemon/Skill/Steal.java
+++ Simon/src/Pokemon/Skill/Steal.java
@@ -8,27 +8,28 @@ import Pokemon.Pokemon;
 
 /**
  *
- * @author asus
+ * @author Jason
  */
-public class Heal extends Skill{
-
-    public Heal() {
-        super("Heal");
+public class Steal extends Skill{
+    
+    public Steal() {
+        super("Steal !");
     }
-
+    
     @Override
     public String use(Pokemon player, Pokemon enemy, boolean usedByPlayer) {
         int heal = player.getDamage() * 10;
         player.healed(heal);
-        
+        int damage = (int) (player.getDamage()*1);
+        enemy.damaged(damage);
         
         if(usedByPlayer){
-            return "Player +" + heal + "HP";
+            return "enemy damaged by " + damage + "Player +" + heal + "HP";
         }else{
-            return "Enemy +" + heal + "HP";
+            return "player damaged by " + damage + "Enemy +" + heal + "HP";
         }
     }
-
+    
     @Override
     public String getDescription() {
         return "This skill will heal the caster based by 1000% of pokemon damage.";
